e7bcedd45b7c8920d56e61d998b26c69c3983d22
A.I <merrick@luois.me>
2017-09-29 10:51:16 +0800

    add android integration

--- android/src/main/java/me/youchai/reactnative/jaq/RNJaqModule.java
+++ android/src/main/java/me/youchai/reactnative/jaq/RNJaqModule.java
@@ -1,22 +1,42 @@
-
 package me.youchai.reactnative.jaq;
+
+import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import com.alibaba.wireless.security.jaq.JAQException;
+import com.alibaba.wireless.security.jaq.SecurityInit;
+
 public class RNJaqModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNJaqModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNJaqModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNJaq";
-  }
+    @Override
+    public String getName() {
+        return "RNJaq";
+    }
+
+    @ReactMethod
+    public void init() {
+        //初始化
+        try {
+            SecurityInit.Initialize(getApplicationContext());
+        } catch (JAQException e) {
+            Log.e("JAQ", "errorCode =" + e.getErrorCode());
+        }
+        SecurityInit.Initialize(reactContext);
+    }
+
+    @ReactMethod
+    public String encryptText(String text) {
+        return text;
+    }
 }
